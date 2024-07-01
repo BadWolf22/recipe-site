0f45349adbc4779e1ade3f034ddf4437c3451131
Matthew Postel <Matthew.Postel@questanalytics.com>
2024-07-01 11:55:17 -0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Create from './pages/create/Create'
+import Home from './pages/home/Home'
+import Recipe from './pages/recipe/Recipe'
+import Search from './pages/search/Search'
+
+// styles
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/create">
+            <Create />
+          </Route>
+          <Route exact path="/recipes/:id">
+            <Recipe />
+          </Route>
+          <Route exact path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
